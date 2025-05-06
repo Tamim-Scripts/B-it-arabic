2c7dde2c94f9babc6530c87f0bc8a8fd9e7e8266
Tamim <tamim193002014@gmail.com>
2025-05-06 14:52:53 +0300

    whatsapp button added commit

--- php/process-form.php
+++ php/process-form.php
@@ -1,20 +1,32 @@
 <?php
-if (isset($_REQUEST['firstname'],$_REQUEST['email'])) {
-      
-    $firstname = $_REQUEST['firstname'];
-	$lastname = $_REQUEST['lastname'];
-    $email = $_REQUEST['email'];
-    $message = $_REQUEST['message'];
-      
-    // Set your email address where you want to receive emails. 
-    $to = 'youremail@mail.com';
-      
+if (isset($_POST['firstname'], $_POST['email'], $_POST['message'])) {
+
+    $firstname = strip_tags(trim($_POST['firstname']));
+    $lastname = strip_tags(trim($_POST['lastname']));
+    $email = filter_var(trim($_POST['email']), FILTER_SANITIZE_EMAIL);
+    $message = strip_tags(trim($_POST['message']));
+
+    // Set your email address where you want to receive emails
+    $to = 'tamim.bit.co@gmail.com';
+
     $subject = 'Contact Request From Website';
-    $headers = "From: ".$firstname." <".$email."> \r\n";
-      
-    $send_email = mail($to,$subject,$message,$headers);
-      
-    echo ($send_email) ? 'success' : 'error';
-      
+    $email_content = "First Name: $firstname\n";
+    $email_content .= "Last Name: $lastname\n";
+    $email_content .= "Email: $email\n\n";
+    $email_content .= "Message:\n$message\n";
+
+    $headers = "From: $firstname <$email>\r\n";
+    $headers .= "Reply-To: $email\r\n";
+
+    if (mail($to, $subject, $email_content, $headers)) {
+        echo 'success';
+    } else {
+        echo 'error';
+    }
+
+} else {
+    echo 'error: missing fields';
 }
 ?>
+
+
